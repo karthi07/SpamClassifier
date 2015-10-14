955b4355b10db19e430e53e1fb5a383b65b12c5e
Karthick <itkarthi02@gmail.com>
2015-10-14 08:56:37 +0530

    create tdm and prob

--- emailclassifier.R
+++ emailclassifier.R
@@ -1,5 +1,5 @@
 setwd("Documents/ML/Classification/SpamClassifier/")
-install.packages("tm")
+#install.packages("tm")
 library(tm)
 spam.path <- 'data/spam'
 easyham.path <- 'data/easyham'
@@ -10,5 +10,47 @@ get.msg <- function(path){
   msg <- text [seq(which(text=="")[1]+1,length(text),1)]
   close(con)
   return(paste(msg,collapse="\n"))
-  
 }
+
+
+spam.docs <- dir(spam.path)
+spam.docs <- spam.docs[which(spam.docs!="cmds")]
+all.spam <- sapply(spam.docs,function(p)get.msg(paste(spam.path,p,sep="/")))
+head(all.spam)
+
+#Creating TDM
+
+get.tdm <- function(doc.vec) {
+  doc.corpus <- Corpus(VectorSource(doc.vec))
+  control <- list(stopwords=TRUE,removePunctuation=TRUE,removeNumbers=TRUE,minDocFreq=2)
+  doc.tdm <- TermDocumentMatrix(doc.corpus,control)
+  
+  return(doc.tdm)
+}
+
+spam.tdm <- get.tdm(all.spam)
+
+
+#prob of terms in msg
+
+spam.matrix <- as.matrix(spam.tdm)
+spam.counts <- rowSums(spam.matrix)
+names(spam.counts)
+spam.df <- data.frame(cbind(names(spam.counts),
+                            as.numeric(spam.counts)),stringsAsFactors=FALSE)
+head(spam.df)
+names(spam.df) <- c('term','frequency') 
+
+str(spam.df)
+spam.df$frequency <- as.numeric(spam.df$frequency)
+
+
+
+
+
+
+
+
+
+
+
